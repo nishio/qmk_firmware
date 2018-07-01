24f311a753a43daca5ca39e532082a79c07a038f
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2018-07-02 01:01:01 +0900

    add arrow keys

--- keyboards/ergodox/keymaps/thumb_shift/keymap.c
+++ keyboards/ergodox/keymaps/thumb_shift/keymap.c
@@ -20,9 +20,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // layer 0: Thumb Shift
 // left hand
 KC_ESC,  M(0),  M(1) , M(2),  M(3),  M(4),  KC_NO,
-KC_TAB,  M(10), M(11), M(12), M(13), M(14), KC_NO,
+KC_TAB,  M(10), M(11), M(12), M(13), M(14), KC_UP,
 KC_TAB,  M(20), M(21), M(22), M(23), M(24),
-KC_LSFT, M(30), M(31), M(32), M(33), M(34), KC_NO,
+KC_LSFT, M(30), M(31), M(32), M(33), M(34), KC_DOWN,
 TO(1),   KC_NO, KC_NO, KC_NO, KC_NO,
 
 /*   */ KC_NO,    KC_NO,
@@ -30,11 +30,11 @@ TO(1),   KC_NO, KC_NO, KC_NO, KC_NO,
 M(100), KC_SPACE, KC_NO,
 
 // right hand
-KC_NO,   M(5),  M(6),  M(7),  M(8),  M(9),  KC_BSPACE,
-KC_NO,   M(15), M(16), M(17), M(18), M(19), KC_NO,
-/*    */ M(25), M(26), M(27), M(28), M(29), KC_NO,
-KC_NO,   M(35), M(36), M(37), M(38), M(39), KC_NO,
-/*           */ KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+KC_NO,   M(5),  M(6),    M(7),     M(8),  M(9),  KC_BSPACE,
+KC_NO,   M(15), M(16),   M(17),    M(18), M(19), KC_NO,
+/*    */ M(25), M(26),   M(27),    M(28), M(29), KC_NO,
+KC_NO,   M(35), M(36),   M(37),    M(38), M(39), KC_NO,
+/*           */ KC_LEFT, KC_RIGHT, KC_NO, KC_NO, KC_NO,
 
 KC_NO, KC_NO,
 KC_NO,
